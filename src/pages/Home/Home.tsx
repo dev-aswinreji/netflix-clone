b1e13152132718b8441e90b8d8c1e353045eb150
Aswin Reji <aswinreji.004@gmail.com>
2024-08-28 14:37:37 +0000

    feat: Integrated tmdb api succesfully and fetched the data

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -29,7 +29,7 @@ const Home = () =>{
             </div>
         </div>
             <div className="more-cards">
-                <TitleCards title={"Blockbuster Movies"}/>
+                <TitleCards title={"Blockbuster Movies"} category={"top_rated"}/>
                 <TitleCards title={"Only on Netflix"}/>
                 <TitleCards title={"Upcoming"}/>
                 <TitleCards title={"Top pics for you"}/>
