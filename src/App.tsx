b4682fef1a52ea4cc96a82e5beba03bd46608f8d
Aswin Reji <aswinreji.004@gmail.com>
2024-08-28 18:48:08 +0000

    feat: firebase auth is done firebase db created using navigate hook changed route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import Home from "./pages/Home/Home"
-import SignIn from "./pages/Signup/Login"
+import Login from "./pages/Login/Login"
 import Player from "./pages/Player/Player"
+import { onAuthStateChanged } from "firebase/auth"
+import { useEffect } from "react"
+import { auth } from './firebase'
 
+const App = () => {
 
-const App = ()=>{
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth,async(user)=>{
+      if(user){
+        console.log("Logged In");
+        navigate('/')
+      }else {
+        console.log("Logged Out");
+        navigate('/login')
+      }
+    })
+  }, [])
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<SignIn/>} />
-        <Route path="/player/:id" element={<Player/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
       </Routes>
     </div>
   )
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,56 @@
+
+import { SiNetflix } from 'react-icons/si'
+import './Login.css'
+import { useState } from 'react'
+import { login, signup } from '../../firebase'
+
+
+const SignIn = () => {
+    const [signState, setSignState] = useState("Sign In")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const user_auth = async (event: Event) => {
+        event.preventDefault()
+        if (signState === 'Sign In') {
+            await login(email, password)
+        } else {
+            await signup(name, email, password)
+        }
+    }
+
+    return (
+        <div className="signin">
+            <span className='signin-logo'>
+                <SiNetflix />
+            </span>
+            <div className="signin-form">
+                <h1>{signState}</h1>
+                <form >
+                    {signState === "Sign Up" ?
+                        <input type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder='Your Name' /> : <></>}
+                    <input type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder='Your Email' />
+                    <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder='Password' />
+                    <button onClick={user_auth} type='submit'>
+                        {signState}
+                    </button>
+                    <div className="form-help">
+                        <div className="remember">
+                            <input type="checkbox" />
+                            <label htmlFor="">Remember Me</label>
+                        </div>
+                        <p>Need Help?</p>
+                    </div>
+                </form>
+                <div className="form-switch">
+                    {signState === 'Sign In' ? <p>New to Netflix? <span onClick={() => { setSignState("Sign Up") }}>Sign Up Now</span></p> :
+
+                        <p>Already have an account? <span onClick={() => { setSignState("Sign In") }}>Sign In Now</span></p>}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn 
