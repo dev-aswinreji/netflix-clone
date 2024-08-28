b94d3f0a030e76793ef4a75461e5cc5be74100c2
Aswin Reji <aswinreji.004@gmail.com>
2024-08-28 11:37:41 +0000

    made ui regarding home page with adding movie banner and description and added play buttonand more info

--- src/components/TitleCards/TitleCards.tsx
+++ src/components/TitleCards/TitleCards.tsx
@@ -1,10 +1,21 @@
 
 import './TitleCards.css'
+import card_data from '../../assets/cards/Cards_data'
 
 
 const TitleCards = () =>{
     return (
-        <div className="titlecards"></div>
+        <div className="titlecards">
+            <h2>Popular on Netflix</h2>
+            <div className="card-list">
+                {card_data.map((card,index)=>{
+                    return <div className="card" key={index}>
+                        <img src={card.image} alt="" />
+                        <p>{card.name}</p>
+                    </div>
+                })}
+            </div>
+        </div>
     )
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
 
 import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
-import hero_banner from '../../../public/hero_banner.jpg'
+import hero_banner from '/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () =>{
     return (
@@ -13,6 +15,14 @@ const Home = () =>{
             <div className="hero-caption">
                 <img src={hero_title} alt="" className='caption-img'/>
                 <p>Discovering his ties to a secret ancient order, a young man livig in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+                <div className="hero-btns">
+                    <button className="btn">
+                        <img src={play_icon} alt="" /> Play
+                    </button>
+                    <button className="btn dark-btn">
+                        <img src={info_icon} alt="" /> More Info
+                    </button>
+                </div>
             </div>
         </div>
         </div>
