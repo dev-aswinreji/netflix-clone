db2a8e292d3bd797e53d892403824bdb26675952
Aswin Reji <aswinreji.004@gmail.com>
2024-08-28 12:58:41 +0000

    feat: Added footer ui and copyright text and icon using react-icons and installed react-router-dom

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,40 @@
-
+import {
+    SiYoutube,
+    SiTwitch,
+    SiInstagram,
+    SiFacebook,
+} from 'react-icons/si'
 import './Footer.css'
 
 const Footer = ()=>{
     return (
-        <div className="footer"></div>
+        <div className="footer">
+            <div className="footer-icons">
+                <SiYoutube/>
+                <SiFacebook/>
+                <SiInstagram/>
+                <SiTwitch/>
+            </div>
+            <ul>
+                <li>
+                    Audio Description
+                </li>
+                <li>Help Center</li>
+                <li>Gift Cards</li>
+                <li>Media Center</li>
+                <li>Investor Relations</li>
+                <li>Jobs</li>
+                <li>Terms and User</li>
+                <li>Privacy</li>
+                <li>Legal Notices</li>
+                <li>Cookie Preferences</li>
+                <li>Corporate Information</li>
+                <li>Contact Us</li>
+            </ul>
+            <p className="copyright-text">&#169; 1997-2024 Netflix, Inc.</p>
+        </div>
     )
 }
 
 export default Footer
+
