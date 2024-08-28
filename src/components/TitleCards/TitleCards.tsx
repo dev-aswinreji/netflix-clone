d9f3682c1e943e2b0bd4332b00e30b974bfff620
Aswin Reji <aswinreji.004@gmail.com>
2024-08-28 12:28:14 +0000

    Added ui for title card and implemented css according to it and make it scrollable using mouse

--- src/components/TitleCards/TitleCards.tsx
+++ src/components/TitleCards/TitleCards.tsx
@@ -1,13 +1,23 @@
 
 import './TitleCards.css'
 import card_data from '../../assets/cards/Cards_data'
+import { useEffect, useRef } from 'react'
 
 
 const TitleCards = () =>{
+const cardsRef = useRef(null);
+const handleWheel= (event:Event) =>{
+
+    event.preventDefault;
+    cardsRef.current.scrollLeft += event.deltaY;
+}
+useEffect(()=>{
+    cardsRef.current.addEventListener('wheel',handleWheel);
+},[])
     return (
-        <div className="titlecards">
+        <div className="title-cards">
             <h2>Popular on Netflix</h2>
-            <div className="card-list">
+            <div className="card-list" ref={cardsRef}>
                 {card_data.map((card,index)=>{
                     return <div className="card" key={index}>
                         <img src={card.image} alt="" />
