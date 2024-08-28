607a98b118066402a7a185f7e83975f021462c8a
Aswin Reji <aswinreji.004@gmail.com>
2024-08-28 11:13:39 +0000

    feat: Added navbar and css and jsx making better layout adding dropdown and hover effect in profile page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,21 @@
 
+import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
+import hero_banner from '../../../public/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
 
 const Home = () =>{
     return (
-        <div className="home"></div>
+        <div className="home">
+        <Navbar/>
+        <div className="hero">
+            <img src={hero_banner} alt="" className='banner-img'/>
+            <div className="hero-caption">
+                <img src={hero_title} alt="" className='caption-img'/>
+                <p>Discovering his ties to a secret ancient order, a young man livig in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+            </div>
+        </div>
+        </div>
     )
 }
 
