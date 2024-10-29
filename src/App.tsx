cc40826e110d1baaf9262a7992bca89f72f77a40
Aswin Reji <aswinreji.004@gmail.com>
2024-10-29 17:52:36 +0000

    feat: Netflix clone dockarized

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
 function App() {
 
+  const word = "New feat"
+
   return (
     <>
     <h1>This is home page</h1>
     <h1>New feature should be implement</h1>
+        {word}
     </>
   )
 }
